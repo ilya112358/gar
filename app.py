fd753d226cdec0d00b11f0ef3b9405b3335354ab
I Bo <ilya112358@gmail.com>
2023-10-29 00:46:54 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.markdown("# Gait Analysis Reports")
+st.write("This is where the data from Visual3D will be imported and visualized")
