cc2d22b1477bd598ae1bbea4c7ee69edf9e595c1
I Bo <ilya112358@gmail.com>
2024-01-02 16:22:00 +0000

    Mean and Consistency

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# import numpy as np
 import os
 import pandas as pd
 import streamlit as st
@@ -7,16 +6,18 @@ st.title("Gait Analysis Report")
 st.text("This is where the data from Visual3D will be imported and visualized")
 
 
+@st.cache_data
 def process_data_file(directory, file):
     """Load file, pre-process data, return dataframe"""
 
-    file_path = os.path.join(directory, file) + ".txt"
+    file_path = os.path.join(directory, file) + '.txt'
     df = pd.read_csv(file_path, sep='\t')
     # Now 'df' is a pandas DataFrame containing data from the file
     # Remove first 4 text rows (headers)
     df = df.drop(df.index[:4])
     for col in df.columns:
         df[col] = pd.to_numeric(df[col], errors='coerce')
+    df.columns = df.columns.str.replace('Gait ', '')
     # The file's 1st col are numbers from 1 to 101
     df.rename(columns={df.columns[0]: 'Gait cycle'}, inplace=True)
     df['Gait cycle'] -= 1
@@ -34,11 +35,17 @@ def plot_graph(file, df):
     """Visualize the dataframe"""
 
     st.header(file)
-    if st.checkbox('Show data', key=file):  # unique key required for the widget
-        st.write(df)
-    st.line_chart(df, x='Gait cycle')
+    if st.checkbox(f"Show gait data for {file}"):
+        st.dataframe(df, hide_index=True)
+    df1 = df[['Gait cycle', 'Mean']]
+    df2 = df.drop('Mean', axis='columns')
+    if st.checkbox(f"Check consistecy for {file}"):
+        st.line_chart(df2, x='Gait cycle')
+    else:
+        st.line_chart(df1, x='Gait cycle')
 
 
+@st.cache_data
 def get_all_files(directory):
     """Get all txt file names in a directory, sort by Left/Right"""
 
@@ -46,7 +53,7 @@ def get_all_files(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             f_name, f_ext = os.path.splitext(file)
-            if f_ext == ".txt":
+            if f_ext == '.txt':
                 file_paths.append(f_name)
     file_paths.sort()
     filesR = [f for f in file_paths if f.startswith('R')]
@@ -60,12 +67,11 @@ def get_all_files(directory):
     files = files_pairs + filesOther
     return files
 
-
 directory = 'Data'
 data_files = get_all_files(directory)
 
 with st.sidebar:
-    st.markdown("[Gait Analysis Report](#gait-analysis-report)")
+    st.markdown("[Go to the Top](#gait-analysis-report)")
     st.write(f"**{len(data_files)}** files found in {directory} folder")
     # Sections list
     for f in data_files:
@@ -77,7 +83,7 @@ with st.sidebar:
                 sec = f[5:]
                 lnk = f.lower().replace(' ', '-').replace('_', '-') 
         elif f.startswith('R'):
-            sec = ""
+            sec = ''
         else:
             sec = f
             lnk = f.lower().replace(' ', '-').replace('_', '-')  
