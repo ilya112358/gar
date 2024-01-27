341e41fab48e5b506c40372d9b21bcc3f19e8426
I Bo <ilya112358@gmail.com>
2024-01-27 19:02:49 +0000

    Add more kinematics. Apply black formatter.

--- app.py
+++ app.py
@@ -16,24 +16,24 @@ def process_data_file(directory, file):
     """Load file, pre-process data, return dataframe"""
 
     file_path = os.path.join(directory, file)
-    df = pd.read_csv(file_path, sep='\t')
+    df = pd.read_csv(file_path, sep="\t")
     # Now 'df' is a pandas DataFrame containing data from the file
     # Remove first 4 text rows (headers)
     df = df.drop(df.index[:4])
     for col in df.columns:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
-    df.columns = df.columns.str.replace('Gait ', '')
-    df.columns = df.columns.str.replace('.c3d', '')
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+    df.columns = df.columns.str.replace("Gait ", "")
+    df.columns = df.columns.str.replace(".c3d", "")
     # The file's 1st col are numbers from 1 to 101
-    df.rename(columns={df.columns[0]: 'Gait cycle'}, inplace=True)
-    df['Gait cycle'] -= 1
+    df.rename(columns={df.columns[0]: "Gait cycle"}, inplace=True)
+    df["Gait cycle"] -= 1
     # Add average over dynamic walks as the last column
-    df1 = df.drop('Gait cycle', axis='columns')
+    df1 = df.drop("Gait cycle", axis="columns")
     try:
-        df1 = df1.drop('Static', axis='columns')
+        df1 = df1.drop("Static", axis="columns")
     except KeyError:  # ['Static.c3d'] not found (e.g., Moment)
         pass
-    df['Mean'] = df1.mean(numeric_only=True, axis=1)
+    df["Mean"] = df1.mean(numeric_only=True, axis=1)
     return df
 
 
@@ -43,10 +43,12 @@ def process_pair(directory, files):
 
     df_left = process_data_file(directory, files[1])
     df_right = process_data_file(directory, files[2])
-    df_left.rename(columns={df_left.columns[-1]: 'Left Mean'}, inplace=True)
-    df_right.rename(columns={df_right.columns[-1]: 'Right Mean'}, inplace=True)
+    df_left.rename(columns={df_left.columns[-1]: "Left Mean"}, inplace=True)
+    df_right.rename(columns={df_right.columns[-1]: "Right Mean"}, inplace=True)
     # combine Gait cycle and two Mean columns to plot both
-    df_both = pd.concat([df_left.iloc[:, :1], df_left.iloc[:, -1:], df_right.iloc[:, -1:]], axis=1)
+    df_both = pd.concat(
+        [df_left.iloc[:, :1], df_left.iloc[:, -1:], df_right.iloc[:, -1:]], axis=1
+    )
 
     def stats(df, col):
         idxmax = df[col].idxmax()
@@ -54,14 +56,16 @@ def process_pair(directory, files):
         idxmin = df[col].idxmin()
         min = f"{df.loc[idxmin, col]:.2f} at {df.loc[idxmin, 'Gait cycle']}%"
         return max, min
-        
-    stats_left = stats(df_left, 'Left Mean')
-    stats_right = stats(df_right, 'Right Mean')
-    df_stats = pd.DataFrame({
-        'Side': ['Left', 'Right'], 
-        'Maximum': [stats_left[0], stats_right[0]], 
-        'Minimum': [stats_left[1], stats_right[1]]
-        })
+
+    stats_left = stats(df_left, "Left Mean")
+    stats_right = stats(df_right, "Right Mean")
+    df_stats = pd.DataFrame(
+        {
+            "Side": ["Left", "Right"],
+            "Maximum": [stats_left[0], stats_right[0]],
+            "Minimum": [stats_left[1], stats_right[1]],
+        }
+    )
     return df_left, df_right, df_both, df_stats
 
 
@@ -69,40 +73,79 @@ def plot_widegraph(bioparameter, dfs):
     """Visualize left and right"""
 
     def plot_df(df, lrb):
-        size = {'height': 600, 'width': 1000}
-        p = figure(x_axis_label='Gait cycle, %',
-                   y_axis_label='Degrees', 
-                   height=size['height'],
-                   width=size['width'],
-                   tools = 'box_zoom, reset',
-                   tooltips = '[$name] @$name{0.00} at @{Gait cycle}')  # [Mean] -0.77 at 33
-        p.border_fill_color = 'seashell'
+        size = {"height": 500, "width": 1000}
+        p = figure(
+            x_axis_label="Gait cycle, %",
+            y_axis_label="Degrees",
+            height=size["height"],
+            width=size["width"],
+            tools="box_zoom, reset",
+            tooltips="[$name] @$name{0.00} at @{Gait cycle}",  # [Mean] -0.77 at 33
+            min_border_top=20,  # Add padding above the plot
+        )
+        p.border_fill_color = "seashell"
         lines, labels = [], []
         for col in range(1, len(df.columns)):
             column = df.columns[col]
-            if column == 'Static':
-                color = 'orange'
-            elif 'Mean' in column and lrb != 'Both':
-                color = 'black'
+            if column == "Static":
+                color = "orange"
+            elif "Mean" in column and lrb != "Both":
+                color = "black"
             else:
-                color = ['red', 'blue', 'green', 'cyan', 'fuchsia', 'blueviolet', 'brown'][col-1]
-            if 'Mean' in column:
+                color = [
+                    "red",
+                    "blue",
+                    "green",
+                    "cyan",
+                    "magenta",
+                    "lime",
+                    "gray",
+                    "navy",
+                    "teal",
+                    "olive",
+                    "purple",
+                    "maroon",
+                    "aqua",
+                    "fuchsia",
+                    "silver",
+                    "blueviolet",
+                    "brown",
+                    "darkorange",
+                    "lightseagreen",
+                    "deeppink",
+                ][col - 1]
+            if "Mean" in column:
                 width = 3
             else:
                 width = 1
-            line = p.line('Gait cycle', column, source=ColumnDataSource(df), 
-                          color=color, width=width, name=column)
+            line = p.line(
+                "Gait cycle",
+                column,
+                source=ColumnDataSource(df),
+                color=color,
+                width=width,
+                name=column,
+            )
             lines.append(line)
             labels.append((column, [line]))
-        legend = Legend(items=labels, location='center')
-        legend.orientation = 'horizontal'
-        legend.border_line_color = 'black'
-        p.add_layout(legend, 'above')        
-        components.html(file_html(p, 'cdn', ), height=size['height'], width=size['width'])
+        legend = Legend(items=labels)
+        # legend.orientation = 'horizontal'
+        legend.border_line_color = "black"
+        p.add_layout(legend, "right")
+        components.html(
+            file_html(
+                p,
+                "cdn",
+            ),
+            height=size["height"],
+            width=size["width"],
+        )
 
     st.header(bioparameter)
-    opts = ['Left', 'Right', 'Both']
-    foot2plot = st.radio(f'Show plot for {bioparameter}', opts, horizontal=True, index=2)
+    opts = ["Left", "Right", "Both"]
+    foot2plot = st.radio(
+        f"Show plot for {bioparameter}", opts, horizontal=True, index=2
+    )
     plot_df(dfs[opts.index(foot2plot)], foot2plot)
     st.dataframe(dfs[3], hide_index=True)
 
@@ -116,9 +159,9 @@ def get_all_files(directory):
     """Get all txt file names in a directory, pair by Left/Right"""
 
     # read from configuration file
-    config_file = 'source.csv'
+    config_file = "source.csv"
     source = []
-    with open(config_file, 'r') as file:
+    with open(config_file, "r") as file:
         reader = csv.reader(file)
         for row in reader:
             # Use the strip method to remove leading and trailing spaces from each element
@@ -127,13 +170,15 @@ def get_all_files(directory):
     # for all rows in source check if row[1] and row[2] exist in directory Data
     file_pairs = []
     for row in source:
-        if os.path.isfile(os.path.join(directory, row[1])) and os.path.isfile(os.path.join(directory, row[2])):
+        if os.path.isfile(os.path.join(directory, row[1])) and os.path.isfile(
+            os.path.join(directory, row[2])
+        ):
             file_pairs.append(row)
     print(f"{len(file_pairs)} pairs loaded")
     return file_pairs
 
 
-directory = 'Data'
+directory = "Data"
 data_files = get_all_files(directory)
 
 st.title("Gait Analysis Report")
@@ -147,7 +192,7 @@ with st.sidebar:
             section = f[0]
         else:
             section = f
-        lnk = section.lower().replace(' ', '-').replace('_', '-')  # slugify
+        lnk = section.lower().replace(" ", "-").replace("_", "-")  # slugify
         st.markdown(f"[{section}](#{lnk})")
     if st.button("Clear Cache"):
         st.cache_data.clear()
