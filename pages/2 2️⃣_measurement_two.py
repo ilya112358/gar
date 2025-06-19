123bef32438ec495817c1812e269ac734a372442
I Bo <ilya112358@gmail.com>
2025-06-19 17:37:03 +0300

    Introduce params category

--- pages/2 2️⃣_measurement_two.py
+++ pages/2 2️⃣_measurement_two.py
@@ -45,46 +45,51 @@ if "d2" in st.session_state:
     st.header("Temporal and Spatial", divider=True)
     # st.dataframe(st.session_state["d2"].ts.fillna(''), hide_index=True)  # removing None (np.nan) leads to a warning due to mixed types
     st.dataframe(st.session_state["d2"].ts, hide_index=True)
-    st.header("Summary Grid", divider=True)
-    PlotLayout(st.session_state["d2"])
-    st.write("[Go to the Top](#measurement-two)")
-    st.header("Interactive Plots", divider=True)
-    Plot(st.session_state["d2"])
-    st.write("[Go to the Top](#measurement-two)")
-    
-    # Write df in Excel format to memory and link to Download button
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    workbook = writer.book
-    worksheet = workbook.add_worksheet()  # writer.sheets['Sheet1']
-    # add title
-    bold = workbook.add_format({'bold': True})  # formatter
-    worksheet.write_string(0, 0, title, bold)
-    row_num = 2
-    # subject info
-    info_df.to_excel(writer, sheet_name='Sheet1', startrow=row_num, index=False)
-    row_num += info_df.shape[0] + 2
-    # temporal and spatial
-    ts = st.session_state["d2"].ts
-    ts.to_excel(writer, sheet_name='Sheet1', startrow=row_num, index=False)
-    row_num += ts.shape[0] + 2
-    # format column width
-    longest_string_length = max(len(str(i)) for i in ts["Parameters"])
-    worksheet.set_column(0, 0, longest_string_length)
-    # add individual stats
-    for param2plot, stats in st.session_state["add_to_rep"].items():
-        worksheet.write_string(row_num, 0, param2plot, bold)
-        row_num += 1
-        stats["df_stats"].to_excel(writer, sheet_name='Sheet1', index=False, startrow=row_num)
-        row_num += len(stats["df_stats"]) + 2
-        worksheet.write_string(row_num, 0, stats["analysis"])
-        row_num += 2
-    # save
-    writer.close()
-    st.download_button(
-        label="Download report.xlsx",
-        data=output.getvalue(),
-        file_name="report.xlsx",
-        mime="application/vnd.ms-excel"
+    category = st.selectbox(
+        "You can choose the category of biomechanical parameters to plot", 
+        ("Kinematics",),
+        index=None,
     )
-    
+    if category == "Kinematics":    
+        st.header(category, divider=True)
+        st.subheader("Summary Grid")
+        PlotLayout(st.session_state["d2"])
+        st.write("[Go to the Top](#measurement-two)")
+        st.subheader("Interactive Plots")
+        Plot(st.session_state["d2"])
+        st.write("[Go to the Top](#measurement-two)")
+        # Write df in Excel format to memory and link to Download button
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        workbook = writer.book
+        worksheet = workbook.add_worksheet()  # writer.sheets['Sheet1']
+        # add title
+        bold = workbook.add_format({'bold': True})  # formatter
+        worksheet.write_string(0, 0, title, bold)
+        row_num = 2
+        worksheet.set_column(0, 0, 20)  # first col width=20
+        # subject info
+        info_df.to_excel(writer, sheet_name='Sheet1', startrow=row_num, index=False)
+        row_num += info_df.shape[0] + 2
+        # temporal and spatial
+        ts = st.session_state["d2"].ts
+        if hasattr(ts, 'to_excel'):  # if T&S were loaded into df
+            ts.to_excel(writer, sheet_name='Sheet1', startrow=row_num, index=False)
+            row_num += ts.shape[0] + 2
+        # add individual stats
+        for param2plot, stats in st.session_state["add_to_rep"].items():
+            worksheet.write_string(row_num, 0, param2plot, bold)
+            row_num += 1
+            stats["df_stats"].to_excel(writer, sheet_name='Sheet1', index=False, startrow=row_num)
+            row_num += len(stats["df_stats"]) + 2
+            worksheet.write_string(row_num, 0, stats["analysis"])
+            row_num += 2
+        # save
+        writer.close()
+        st.download_button(
+            label="Download report.xlsx",
+            data=output.getvalue(),
+            file_name="report.xlsx",
+            mime="application/vnd.ms-excel"
+        )
+        
